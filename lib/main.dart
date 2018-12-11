72ce778f00416461e8f81981018572172cf6101d
xuyang <xymelon@163.com>
2018-12-11 20:20:16 +0800

    add: async\image_picker\validation demo

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/count.dart';
 import 'package:flutter_app/list.dart';
 import 'package:flutter_app/signature.dart';
+import 'package:flutter_app/async.dart';
+import 'package:flutter_app/validation.dart';
+import 'package:flutter_app/image_picker.dart';
 
 //void main() => runApp(CountApp());
-void main() => runApp(SignatureApp());
+//void main() => runApp(SignatureApp());
 //void main() => runApp(ListApp());
+//void main() => runApp(AsyncApp());
+//void main() => runApp(ValidationApp());
+void main() => runApp(ImagePickerApp());
